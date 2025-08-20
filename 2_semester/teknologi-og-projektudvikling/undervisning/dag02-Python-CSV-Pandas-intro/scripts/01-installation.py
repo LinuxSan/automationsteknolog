c2128f8f315842330151b22c82aacd48abcfb31a
Anders S. Østergaard <sandoe_1@hotmail.com>
2025-08-20 07:02:51 +0000

    undervisnings folder

--- 2_semester/teknologi-og-projektudvikling/undervisning/dag02-Python-CSV-Pandas-intro/scripts/01-installation.py
+++
@@ -1,5 +0,0 @@
-# 01-installation.py
-# Installation af pandas og læsning af version
-
-import pandas as pd
-print("Pandas version:", pd.__version__)
--- 2_semester/teknologi-og-projektudvikling/undervisning/dag02-Python-CSV-Pandas-intro/scripts/07-next-steps.py
+++
@@ -1,5 +0,0 @@
-# 07-next-steps.py
-# Videre med Pandas og data science
-
-print("Du kan nu arbejde videre med Pandas, NumPy og Matplotlib!")
-print("Prøv fx at lave egne analyser, visualiseringer og data cleaning på dine egne datasæt.")
