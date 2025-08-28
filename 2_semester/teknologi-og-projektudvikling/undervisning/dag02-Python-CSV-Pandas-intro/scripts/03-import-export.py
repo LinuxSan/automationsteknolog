449c3c0227acef3c89692f98a32b4a1e2a605dd1
Anders S. Østergaard <sandoe_1@hotmail.com>
2025-08-28 21:21:57 +0200

    Update 03-import-export.py

--- 2_semester/teknologi-og-projektudvikling/undervisning/dag02-Python-CSV-Pandas-intro/scripts/03-import-export.py
+++ 2_semester/teknologi-og-projektudvikling/undervisning/dag02-Python-CSV-Pandas-intro/scripts/03-import-export.py
@@ -1,6 +1,7 @@
 import pandas as pd
 data = pd.read_csv('Teknolog/02-semester/teknologi-og-projektudvikling/03-præsentationer/dag02-csv-pandas/01-kort-version/python/sensor.csv')
-print(data.head(5))
+#print(data.head(5))
 data = data[data['temperature']>24]
 print(data['temperature'])
 
+pd.DataFrame(data['temperature']).to_csv('Teknolog/02-semester/teknologi-og-projektudvikling/03-præsentationer/dag02-csv-pandas/01-kort-version/python/filtered_temperature.csv', index=False)
