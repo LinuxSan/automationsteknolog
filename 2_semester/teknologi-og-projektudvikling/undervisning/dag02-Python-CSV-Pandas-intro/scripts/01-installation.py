4ea773bd0f24eab8017bb13d9e1f4e26d1db7a8c
Anders S. Østergaard <sandoe_1@hotmail.com>
2025-08-28 21:00:39 +0200

    Update 01-installation.py

--- 2_semester/teknologi-og-projektudvikling/undervisning/dag02-Python-CSV-Pandas-intro/scripts/01-installation.py
+++ 2_semester/teknologi-og-projektudvikling/undervisning/dag02-Python-CSV-Pandas-intro/scripts/01-installation.py
@@ -1,3 +1,10 @@
+''' 
+1. Åben vs code
+2. start ny terminal
+3. tryk ctrl + C på -> pip install pandas matplotlib
+3. gå til terminal og tryk ctrl + V
+'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,3 +13,4 @@ data = pd.DataFrame({"x": [1, 2, 3], "y": [1, 4, 9]})
 print(data)
 data.plot(x="x", y="y", kind="line")
 plt.show()
+
